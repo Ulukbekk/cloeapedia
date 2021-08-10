5196917bbdd042d7fafa1d76495cad41d85985be
Ulukbekk <ulukbekmomunkulov@gmail.com>
2021-08-10 18:11:13 +0600

    added comments and rating

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from blog.models import Post
+from blog.models import Post, Coment
 
 
 class PostCreateForm(forms.ModelForm):
@@ -13,4 +13,10 @@ class PostUpdateForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('category', 'title', 'post', 'image')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Coment
+        fields = ('name', 'text')
+
 
