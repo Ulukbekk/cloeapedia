55d576da93b370f3829e8f2dffc4871710abf57d
Ulukbekk <ulukbekmomunkulov@gmail.com>
2021-08-11 17:40:24 +0600

    final commit

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,6 +1,16 @@
-from .models import Category
+from .models import Category, Post
 
 
 def get_categories(request):
     categories = Category.objects.all()
     return {'categories': categories}
+
+def footer_data(request):
+    recent_posts = Post.objects.all().order_by('-date_created')[:3]
+    popular_posts = Post.objects.all().order_by('-rating')[:3]
+    popular_categories = Post.objects.all()
+    return{
+        'recent_posts':recent_posts,
+        'popular_posts':popular_posts,
+        'popular_categories':popular_categories,
+    }
